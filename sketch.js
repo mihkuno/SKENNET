f7d9f09ecafb0a750bea1ffbace673ea749a9699
mihkuno <caindayjoeninyo@gmail.com>
2023-01-24 11:29:18 +0800

    added color variation

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-
+let model;
+let targetColor = [255,0,0];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -12,8 +13,25 @@ function setup() {
     model = ml5.neuralNetwork(options);
 }
 
+function keyPressed() {
+    switch(key) {
+        case '1': //r
+            targetColor = [255, 0, 0];
+            print('RED');
+            break;
+        case '2': //g
+            targetColor = [0, 255, 0];
+            print('GREEN');
+            break;
+        case '3': //b
+            targetColor = [0, 0, 255];
+            print('BLUE');
+            break;
+    }
+}
+
 function mousePressed() {
     stroke(0);
-    fill(180,0,0);
+    fill(...targetColor);
     circle(mouseX,mouseY,80);
 }
